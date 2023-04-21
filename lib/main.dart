d1e70aff97ffdb7906ee447bff22b28ac71c48b1
WindowsCrashed <windowscrashed0000@gmail.com>
2023-04-21 17:51:40 -0300

    created mock database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app_uni/pages/home.dart';
+import 'package:todo_app_uni/services/seed_db.dart';
 
 void main() {
+  seedDb();
+
   runApp(const MaterialApp(
     title: 'JustDoIi!',
     home: Home(),
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,40 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app_uni/models/task_priority.dart';
-import 'package:todo_app_uni/models/task_type.dart';
 import 'package:todo_app_uni/widgets/app_drawer.dart';
 import 'package:todo_app_uni/widgets/task_card.dart';
-
-import '../models/task.dart';
+import 'package:todo_app_uni/data/db.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<Task> tasks = [
-      Task(
-          name: 'Do some stuff',
-          priority: TaskPriority(priority: 'low', color: Colors.green),
-          types: [TaskType('general')],
-          date: DateTime.now(),
-          description: 'Something goes here...'
-      ),
-      Task(
-          name: 'Clean the house',
-          priority: TaskPriority(priority: 'medium', color: Colors.yellow.shade600),
-          types: [TaskType('cleaning'), TaskType('house chores')],
-          date: DateTime.now(),
-          description: 'You have to clean the house...'
-      ),
-      Task(
-          name: 'Have lunch',
-          priority: TaskPriority(priority: 'high', color: Colors.red, icon: Icons.warning_amber),
-          types: [TaskType('meal')],
-          date: DateTime.now(),
-          description: 'You have to eat... now...'
-      ),
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Just do it!',
@@ -49,11 +22,7 @@ class Home extends StatelessWidget {
       drawer: const AppDrawer(),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
-        children: [
-          TaskCard(task: tasks[0],),
-          TaskCard(task: tasks[1],),
-          TaskCard(task: tasks[2],),
-        ],
+        children: Db.tasks.map((t) => TaskCard(task: t)).toList(),
       ),
     );
   }
