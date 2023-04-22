3775b1e0b3b25b205f8413d302e938385092bb70
WindowsCrashed <windowscrashed0000@gmail.com>
2023-04-21 22:47:20 -0300

    Implemented creation of task types

--- lib/pages/create_task_type.dart
+++ lib/pages/create_task_type.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_uni/models/validation_response.dart';
 import 'package:todo_app_uni/widgets/app_appbar.dart';
+import 'package:todo_app_uni/services/task_type_service.dart';
 
 class CreateTaskType extends StatefulWidget {
   const CreateTaskType({Key? key}) : super(key: key);
@@ -10,8 +12,24 @@ class CreateTaskType extends StatefulWidget {
 
 class _CreateTaskTypeState extends State<CreateTaskType> {
   final _formKey = GlobalKey<FormState>();
+  final _taskTypeService = TaskTypeService();
 
   final TextEditingController fieldController = TextEditingController();
+
+  void save() {
+    if (_formKey.currentState!.validate()) {
+      FocusScope.of(context).unfocus();
+      ValidationResponse response = _taskTypeService.addTaskType(fieldController.text);
+      if (response.response) {
+        Navigator.pop(context);
+      }
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(response.message)));
+    }
+  }
+
+  void cancel() {
+    Navigator.pop(context);
+  }
 
   @override void dispose() {
     fieldController.dispose();
@@ -29,21 +47,20 @@ class _CreateTaskTypeState extends State<CreateTaskType> {
           margin: const EdgeInsets.symmetric(vertical: 80, horizontal: 25),
           child: GestureDetector(
             child: Column(
-              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 70),
-                  child: const Text('Create a new task type',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold
-                    ),
+                const Text('Create a new task type',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
                   ),
+                ),
+                const SizedBox(
+                  height: 100,
                 ),
                 Form(
                   key: _formKey,
                   child: SizedBox(
-                    height: 150,
+                    height: 220,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -60,23 +77,45 @@ class _CreateTaskTypeState extends State<CreateTaskType> {
                             return null;
                           },
                         ),
-                        ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text(fieldController.text))
-                                );
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black,
-                              padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 12),
-                            ),
-                            child: const Text('Create',
-                              style: TextStyle(
-                                fontSize: 20
+                        SizedBox(
+                          height: 110,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                height: 50,
+                                width: 210,
+                                child: ElevatedButton(
+                                    onPressed: save,
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.black,
+                                      padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 12),
+                                    ),
+                                    child: const Text('Save',
+                                      style: TextStyle(
+                                        fontSize: 20
+                                      ),
+                                    )
+                                ),
                               ),
-                            )
+                              SizedBox(
+                                height: 50,
+                                width: 210,
+                                child: OutlinedButton(
+                                    onPressed: cancel,
+                                    style: OutlinedButton.styleFrom(
+                                      foregroundColor: Colors.black54,
+                                      side: const BorderSide(width: 2, color: Colors.black54)
+                                    ),
+                                    child: const Text('Cancel',
+                                      style: TextStyle(
+                                          fontSize: 20
+                                      ),
+                                    )
+                                ),
+                              ),
+                            ],
+                          ),
                         )
                       ],
                     ),
