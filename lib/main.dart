8cdea2d096878682df9d6c3a68e8dff47c019262
WindowsCrashed <windowscrashed0000@gmail.com>
2023-04-23 23:59:31 -0300

    Fixed overflow in card

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
     title: 'JustDoIi!',
     initialRoute: '/home',
     routes: {
-      '/home': (context) => Home(),
+      '/home': (context) => const Home(),
       '/create-task-type': (context) => const CreateTaskType(),
       '/create-task-priority': (context) => const CreateTaskPriority(),
       '/create-task': (context) => const CreateTask()
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,9 +4,14 @@ import 'package:todo_app_uni/widgets/app_drawer.dart';
 import 'package:todo_app_uni/widgets/task_card.dart';
 import 'package:todo_app_uni/services/task_service.dart';
 
-class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final _taskService = TaskService();
 
   @override
@@ -15,6 +20,7 @@ class Home extends StatelessWidget {
       appBar: const AppAppBar(title: 'Just do it!'),
       backgroundColor: Colors.grey[200],
       drawer: const AppDrawer(),
+      onDrawerChanged: (value) { setState(() {}); },
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
         children: _taskService.getTasks().map((t) => TaskCard(task: t)).toList(),
